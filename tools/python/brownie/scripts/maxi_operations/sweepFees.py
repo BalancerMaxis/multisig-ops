6508f31df29d5f2badc6e2985d89491081895759
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-02-03 01:18:02 +0100

    moved input/output directory somewhere easier to find.

--- tools/python/brownie/scripts/maxi_operations/sweepFees.py
+++ tools/python/brownie/scripts/maxi_operations/sweepFees.py
@@ -8,11 +8,12 @@ from dotmap import DotMap
 from datetime import date
 
 today = date.today()
+target_dir = "../../../Feeswap/"
 def main():
     # sweeps is a map with addresses as keys and sweep amounts as values
     sweeps = {}
     report = ""
-    with open(f"FeeData/{today}.json", "r") as f:
+    with open(f"{target_dir}{today}.json", "r") as f:
         data = json.load(f)
     if chain.id == 1:
         sweep_limit = 10000
@@ -34,16 +35,16 @@ def main():
     print(report)
 
     # Generate JSON
-    with open("FeeData/feeSweep.json", "r") as f:
+    with open(f"{target_dir}feeSweep.json", "r") as f:
         tx_builder = json.load(f)
     tx_out_map = DotMap(tx_builder)
     tx_out_map.chainId = chain.id
     # TX builder wants lists in a string, addresses unquoted, and large integers without e+
     tx_out_map.transactions[0].contractInputsValues.tokens = str(list(sweeps.keys())).replace("'", "")
     tx_out_map.transactions[0].contractInputsValues.amounts = str(list(sweeps.values()))
-    with open(f"FeeData/out/{today}.json", "w") as f:
+    with open(f"{target_dir}out/{today}.json", "w") as f:
         json.dump(dict(tx_out_map), f)
-    with open(f"FeeData/out/{today}.report.txt", "w") as f:
+    with open(f"{target_dir}out/{today}.report.txt", "w") as f:
         f.write(report)
 
 
