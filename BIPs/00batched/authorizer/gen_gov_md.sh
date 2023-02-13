e909abba531fc2d00e0c67456ea2226db077e333
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-02-13 19:22:24 +0100

    few more fixes.

--- BIPs/00batched/authorizer/gen_gov_md.sh
+++ BIPs/00batched/authorizer/gen_gov_md.sh
@@ -10,8 +10,8 @@ BIP_DIR="../../BIP-$BIP_NUMBER"
 echo "Creating $BIP_DIR and moving generated files into place."
 mkdir $BIP_DIR
 mv ${date}.json ${BIP_DIR}/inputs.json
-mv ${date}_address_sorted.md ${BIP_DIR}/result_address_sorted.md
-mv ${date}_deployment_sorted.md ${BIP_DIR}/result_deloyment_sorted.md
+mv ${date}_address_sorted.md ${BIP_DIR}/results_address_sorted.md
+mv ${date}_deployment_sorted.md ${BIP_DIR}/results_deloyment_sorted.md
 mv ${date}_function_descriptions.md ${BIP_DIR}/function_descriptions.md
 
 ## move chain jsons
@@ -24,16 +24,17 @@ rm -r chains
 
 ## Build basic governance MD
 
-TABLE=${BIP_DIR}/result_address_sorted.md
+TABLE=${BIP_DIR}/results_address_sorted.md
 
 echo "Building governance forum md file.  Note you will need to review and update the top sections to talk a bit about the change and it's reasons."
 sed "s/XXX/$BIP_NUMBER/g" governance_template.md > .working.md
 sed "s/YYY/$PR_NUMBER/g" .working.md > .working1.md
-sed "/ADDRESS_SORTED_MD_TABLE/r $TABLE" .working1.md | sed 's/ADDRESS_SORTED_MD_TABLE//' > ${BIP_DIR}/BIP-${BIP_NUMBER}.md
+sed "/ADDRESS_SORTED_MD_TABLE/r $TABLE" .working1.md | sed 's/ADDRESS_SORTED_MD_TABLE//' > README.md
 
 rm .working*.md
 
 git add $BIP_DIR/*
+git add -u # find moved files
 git commit -m "Setting up Payload Directory."
 git pull
 git push origin
