47191246c5bb5c83d4cf01ea9a905a3df1ab8c52
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-02-26 17:26:26 +0100

    We decided not to send gas to signers.  So just add tritium as deployer.

--- tools/python/brownie/scripts/maxi_operations/set_gas_station_watchlist.py
+++ tools/python/brownie/scripts/maxi_operations/set_gas_station_watchlist.py
@@ -11,19 +11,11 @@ def main():
     signers = r.balancer.signers.maxis
     deployers = r.balancer.deployers
 
-    for name,address in signers.items():
-        if name in deployers.keys():
+    for name,address in deployers.items():
             # Deployers get more gas in the deployer but no signer gas
-            addresses.append(deployers[name])
+            addresses.append(address)
             topup_amounts.append(int(0.5*10**18))  # .5 ETH  topup for deployers
             min_topups.append(int(0.2*10**18))  # .2 ETH min topup = always has atleast .3
-        else:
-            # This person only has a signer address so gas them less
-            addresses.append(address)
-            topup_amounts.append(int(0.25*10**18)) # .25 eth for signers
-            min_topups.append(int(0.15*10**18))  # always has at least 0.1
 
-    gs = safe.contract(r.balancer.maxi_gas_station)
-    gs.setWatchList(addresses, topup_amounts, min_topups)
     # adding 1 deployer and 3 signers requires a total of 1.25 eth to topup all new signers to from empty to full
     safe.post_safe_tx(gen_tenderly=False)
