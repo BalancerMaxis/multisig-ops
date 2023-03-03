70513a3ddaa8f283da4da8eba7e1ab8fce6465b6
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-03-03 08:59:44 -0700

    add Goerli rpc.

--- tools/python/gen_add_permissions_payload.py
+++ tools/python/gen_add_permissions_payload.py
@@ -21,7 +21,8 @@ w3_by_chain = {
     "arbitrum": Web3(Web3.HTTPProvider(f"https://arbitrum-mainnet.infura.io/v3/{INFURA_KEY}")),
     "optimism": Web3(Web3.HTTPProvider(f"https://optimism-mainnet.infura.io/v3/{INFURA_KEY}")),
     "polygon": Web3(Web3.HTTPProvider(f"https://polygon-mainnet.infura.io/v3/{INFURA_KEY}")),
-    "gnosis": Web3(Web3.HTTPProvider(f"https://rpc.gnosischain.com/"))
+    "gnosis": Web3(Web3.HTTPProvider(f"https://rpc.gnosischain.com/")),
+    "polygon": Web3(Web3.HTTPProvider(f"https://goerli.infura.io/v3/{INFURA_KEY}")),
 }
 
 
