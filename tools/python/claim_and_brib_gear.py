4026b4f5f0e7b05ad6d7abdb6e47f936e56c8707
Tritium <11885129+Tritium-VLK@users.noreply.github.com>
2023-03-16 22:18:51 +0100

    Generate Gearbox Claim payload (#96)
    * Inputs to generate transactions to give the poolRecoveryHelper ability to put V3 pools into recovery mode
    
    * Update generation script to handle poolRecoveryHelper tha is not a multisig and not in r.balancer.multisig.
    
    * Generate permissions add payload and tables
    
    * Setting up Payload Directory.
    
    * Update BIP text
    
    * reformatted json files as the prettier broke again.
    
    * reformatted json files as the prettier broke again.
    
    * Reformatted JSON files
    
    * Build transaction to claim gear (test/example)
    
    * Reformatted JSON files
    
    * Use proper multisig as sender and claimer.
    
    * - tested first run and generated json to successfully claimed|
    - started working on brib logic, blocked by missing item in labels map.
    - abstracted some of the bribe handling logic from the brownie code into non-brownie land.
    
    * Use main branch for labels.
    
    * Working on getting the first claim to simulate.  Looks good now, just need whitelist.
    
    * Successfully Loaded tx into safe for first run.
    
    ---------
    
    Co-authored-by: Github Actions <noreply@users.noreply.github.com>
    Co-authored-by: Tritium-VLK <Tritium-VLK@users.noreply.github.com>

---
+++ tools/python/claim_and_brib_gear.py
@@ -0,0 +1,101 @@
+import json
+import requests
+from dotmap import DotMap
+from datetime import date
+from web3 import Web3
+import os
+from helpers.addresses import get_registry_by_chain_id
+import binascii
+from helpers.hh_bribs import get_index, get_gauge_name_map, get_hh_aura_target
+
+today = str(date.today())
+debug = False
+
+r = get_registry_by_chain_id(1)
+
+###TODO: The below settings must be set before running the script
+INFURA_KEY = os.getenv('WEB3_INFURA_PROJECT_ID')
+GEARBOX_MERKLE_URL = "https://raw.githubusercontent.com/Gearbox-protocol/rewards/master/merkle/"
+GEARBOX_TREE="0xA7Df60785e556d65292A2c9A077bb3A8fBF048BC"
+GAUGE_TO_BRIB="0x19A13793af96f534F0027b4b6a3eB699647368e7" ## bb-g-usd
+w3 = Web3(Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{INFURA_KEY}"))
+
+
+
+def sinlge_quote_list_string(list):
+    # TX builder wants lists in a string, addresses unquoted
+    output  = str(list).replace('"', "")
+    return output
+
+
+def claim(claim_address, tree_address):
+    tree = w3.eth.contract(address=tree_address,abi=json.load(open("./abis/GearAirdropDistributor.json")))
+    b = tree.functions.merkleRoot().call()
+    current_root =  binascii.hexlify(b).decode('utf-8')
+    try:
+        result = requests.get(f"{GEARBOX_MERKLE_URL}/mainnet_{current_root}.json")
+    except requests.exceptions.HTTPError as error:
+        print(result.request.url)
+        print(error)
+        assert(False)
+    result = result.json()
+    claim = result["claims"][claim_address]
+    index = claim["index"]
+    amount = int(claim["amount"], 0)
+    proofs = claim["proof"]
+
+    #tree.claim(index, claim_address,amount, noquote_list_string(proofs))
+    with open("tx_builder_templates/gearbox_claim_tx.json", "r") as f:
+        data = DotMap(json.load(f))
+    tx = data.transactions[0]
+    tx.contractInputsValues.index = str(index)
+    tx.contractInputsValues.account = claim_address
+    tx.contractInputsValues.totalAmount = str(amount)
+    tx.contractInputsValues.merkleProof = str(proofs)
+    return tx.toDict()
+
+
+def approve(token, spender, raw_amount):
+    with open("tx_builder_templates/approve.json", "r") as f:
+        tx = json.load(f)
+    tx["to"] = token
+    tx["contractInputsValues"]["spender"] = spender
+    tx["contractInputsValues"]["rawAmount"] = raw_amount
+    return tx
+
+def bribe_aura(gauge_address, bribe_token_address, amount):
+    briber = w3.eth.contract(address=r.hidden_hand.aura_briber,abi=json.load(open("./abis/IAuraBriber.json")))
+    target_name = get_gauge_name_map()[Web3.toChecksumAddress(gauge_address)]
+    prop = get_hh_aura_target(target_name)
+    with open("tx_builder_templates/bribe_aura.json", "r") as f:
+        data = json.load(f)
+    tx = data["transactions"][0]
+    tx["contractInputsValues"]["proposal"] = prop
+    tx["contractInputsValues"]["token"] = bribe_token_address
+    tx["contractInputsValues"]["amount"] = str(amount)
+    return tx
+
+
+def main():
+    ### Claim
+    print(claim(r.balancer.multisigs.lm, GEARBOX_TREE))
+    with open("tx_builder_templates/base.json", "r") as f: ## framework transaction
+        data = json.load(f)
+    claim_tx = claim(r.balancer.multisigs.lm, GEARBOX_TREE)
+    data["transactions"].append(claim_tx)
+
+    ### Bribe
+    claim_amount = claim_tx["contractInputsValues"]["totalAmount"]
+    print(f"Total CLaim {int(claim_amount)/10**18} GEAR")
+    approve_tx = approve(r.tokens.GEAR, r.hidden_hand.bribe_vault, claim_amount)
+    bribe_tx = bribe_aura(gauge_address=GAUGE_TO_BRIB, bribe_token_address=r.tokens.GEAR, amount=claim_amount)
+    data["transactions"].append(approve_tx)
+    data["transactions"].append(bribe_tx)
+
+    data["meta"]["createdFromSafeAddress"] = r.balancer.multisigs.lm
+    with open("gear_example_tx.json", "w") as f: ## framework transaction
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/python/helpers/hh_bribs.py
@@ -0,0 +1,77 @@
+import requests
+from decimal import Decimal, InvalidOperation
+from web3 import Web3
+import csv
+
+
+SNAPSHOT_URL = "https://hub.snapshot.org/graphql?"
+HH_API_URL = "https://api.hiddenhand.finance/proposal"
+
+GAUGE_MAPPING_URL = "https://raw.githubusercontent.com/aurafinance/aura-contracts/main/tasks/snapshot/gauge_choices.json"
+
+# queries for choices and proposals info
+QUERY_PROPOSAL_INFO = """
+query ($proposal_id: String) {
+  proposal(id: $proposal_id) {
+    choices
+  }
+}
+"""
+
+# `state: "all"` ensures all proposals are included
+QUERY_PROPOSALS = """
+query {
+  proposals(first: 100, where: { space: "gauges.aurafinance.eth" , state: "all"}) {
+    id
+  }
+}
+"""
+
+def get_hh_aura_target(target_name):
+    response = requests.get(f"{HH_API_URL}/aura")
+    options = response.json()["data"]
+    for option in options:
+        if option["title"] == target_name:
+            return option["proposalHash"]
+    return False  ## return false if no result
+
+def get_gauge_name_map(map_url=GAUGE_MAPPING_URL):
+    ## the url was not responding on IPv6 addresses
+    requests.packages.urllib3.util.connection.HAS_IPV6 = False
+    response = requests.get(map_url)
+    item_list = response.json()
+    output = {}
+    for mapping in item_list:
+        gauge_address = Web3.toChecksumAddress(mapping["address"])
+        output[gauge_address] = mapping["label"]
+    return output
+
+def get_index(proposal_id, target):
+    # grab data from the snapshot endpoint re proposal choices
+    response = requests.post(
+        SNAPSHOT_URL,
+        json={
+            "query": QUERY_PROPOSAL_INFO,
+            "variables": {"proposal_id": proposal_id},
+        },
+    )
+    choices = response.json()["data"]["proposal"]["choices"]
+    choice = choices.index(target)
+    return choice
+
+def process_bribe_csv(
+       csv_file
+):
+    # Process the CSV
+    # csv_format: target, platform, amount
+    bribe_csv = csv.DictReader(open(csv_file))
+    aura_bribes = []
+    balancer_bribes = []
+    bribes = {
+        "aura": {},
+        "balancer": {}
+    }
+    ## Parse briibes per platform
+    for bribe in bribe_csv:
+        bribes[bribe["platform"]][bribe["target"]] = float(bribe["amount"])
+    return bribes
