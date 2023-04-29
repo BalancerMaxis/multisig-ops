4a9100f96badd7d9deea4670576262950de9f763
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-04-29 20:10:36 +0200

    restructure scripts.

---
+++ action-scripts/brownie/scripts/rerun_all_gauge_reports.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import glob
+from scripts.get_gauge_mappings import gen_report, get_payload_list
+
+debug = False
+
+
+
+
+def main():
+    all_bips = glob.glob("../../BIPs/**/*.json", recursive=True)
+    runlist = []
+    for file in all_bips:
+        runlist.append(file.replace("../../",""))
+    reports = gen_report(runlist)
+    #reports = gen_report(["../../BIPs/BIP-231-241A.json"])
+    #reports = gen_report(get_payload_list())
+    ### Generate comment output
+    with open("output.txt", "w") as f:
+        for report in reports:
+            f.write(report)
+    ### Generate output files
+    for report in reports:
+        filename = Path(f"{report.splitlines()[0]}")
+        filename = filename.with_suffix(".report.txt")
+        with open(f"{filename}", "w") as f:
+            f.write(report)
+
+
+if __name__ == "__main__":
+    main()
