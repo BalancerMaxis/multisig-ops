e3f7cc84ea286b98b6d255b60358c32618c79ff3
Tritium <tritium-vlk@telegram.works>
2023-10-09 21:25:41 +0200

    Fix merge and reporting for data_only_payloads.

--- action-scripts/merge_pr_jsons.py
+++ action-scripts/merge_pr_jsons.py
@@ -204,7 +204,7 @@ def main():
                         "origin_file_name": file['file_name'],
                         "bip_number": extract_bip_number(file),
                     }
-                    if "contractMethod" not in tx.keys():
+                    if "contractMethod" in tx.keys():
                         if tx["contractMethod"]["name"] == "addGauge":
                             try:
                                 gauge_chain = tx["contractInputsValues"]["gaugeType"]
