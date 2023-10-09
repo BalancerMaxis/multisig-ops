67566681b13628648e1c6f543ba417d04af14302
Tritium <11885129+Tritium-VLK@users.noreply.github.com>
2023-10-10 00:58:18 +0200

    Check zkevm injectors every 5 mins and run performUpkeep if required (#541)

--- action-scripts/brownie/scripts/pokeInjectors.py
+++ action-scripts/brownie/scripts/pokeInjectors.py
@@ -2,18 +2,23 @@ from bal_addresses import AddrBook, BalPermissions, MultipleMatchesError, NoResu
 from brownie import network, accounts, Contract
 import os
 
-## LOAD wallet
-mnemonic =  os.environ["GITHUB_REPOSITORY"]
-account = accounts.from_mnemonic(mnemonic)
-print(f"Keeper Address: {account.address}")
-## ZKEVM
-network.disconnect()
-network.connect("zkevm-main")
-book = AddrBook("zkvem")
-injectors =  book.extras.maxiKeepers.gaugeRewardsInjectors.values()
-for injectorAddress in injectors:
-    injector = Contract(injectorAddress)
-    (ready, performdata) = injector.checkUpkeep(b"")
-    if ready:
-        injector.performUpkeep(performdata, {"from": account})
+def main():
+    ## LOAD wallet
+    mnemonic =  os.environ["KEYWORDS"]
+    account = accounts.from_mnemonic(mnemonic)
+    print(f"Keeper Address: {account.address}")
+    ## ZKEVM
+    network.disconnect()
+    network.connect("zkevm-main")
+    book = AddrBook("zkevm")
+    injectors =  book.extras.maxiKeepers.gaugeRewardsInjectors.values()
+    for injectorAddress in injectors:
+        injector = Contract(injectorAddress)
+        (ready, performdata) = injector.checkUpkeep(b"")
+        if ready:
+            injector.performUpkeep(performdata, {"from": account})
+        else:
+            print(f"{injector.address} not ready")
 
+if __name__ == "__main__":
+    main()
