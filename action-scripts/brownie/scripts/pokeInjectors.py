ed0640c689f07512bfd6998d1bb1123068f96830
Tritium <tritium-vlk@telegram.works>
2023-10-10 00:32:03 +0200

    zkevm injector poker

---
+++ action-scripts/brownie/scripts/pokeInjectors.py
@@ -0,0 +1,18 @@
+from bal_addresses import AddrBook, BalPermissions, MultipleMatchesError, NoResultError
+from brownie import network, accounts, Contract
+import os
+
+## LOAD wallet
+mnemonic =  os.environ["GITHUB_REPOSITORY"]
+account = accounts.from_mnemonic(mnemonic)
+## ZKEVM
+network.disconnect()
+network.connect("zkevm-main")
+book = AddrBook("zkvem")
+injectors =  book.extras.maxiKeepers.gaugeRewardsInjectors.values()
+for injectorAddress in injectors:
+    injector = Contract(injectorAddress)
+    (ready, performdata) = injector.checkUpkeep(b"")
+    if ready:
+        injector.performUpkeep(performdata, {"from": account})
+
