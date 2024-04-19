d32daef7c8a9ef43e143f64f320820d92ea6e7c2
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-04-19 13:46:38 +0400

    fix: graceful handling of bugs in tenderly sim generation

--- action-scripts/brownie/scripts/script_utils.py
+++ action-scripts/brownie/scripts/script_utils.py
@@ -357,13 +357,17 @@ def format_into_report(
         )
     )
     file_report += f"CHAIN(S): `{', '.join(chains)}`\n"
-    tenderly_url, tenderly_success = run_tenderly_sim(
+    try:
+        tenderly_url, tenderly_success = run_tenderly_sim(
         file["chainId"], file["meta"]["createdFromSafeAddress"], file["transactions"]
-    )
-    if tenderly_success:
-        file_report += f"TENDERLY: [SUCCESS]({tenderly_url})\n"
-    else:
-        file_report += f"TENDERLY: [FAILURE]({tenderly_url})\n"
+        )
+        if tenderly_success:
+            file_report += f"TENDERLY: [SUCCESS]({tenderly_url})\n"
+        else:
+            file_report += f"TENDERLY: [FAILURE]({tenderly_url})\n"
+    except:
+        file_report += "TENDERLY: [SKIPPED]\n"
+
     file_report += "```\n"
     file_report += convert_output_into_table(transactions)
     file_report += "\n```\n"
@@ -557,5 +561,3 @@ def prettify_gauge_list(gauge_addresses, chainbook) -> list:
                 gauge_name = "(N/A)"
         pretty_gauges.append(f"{gauge} ({gauge_name})")
     return pretty_gauges
-
-
