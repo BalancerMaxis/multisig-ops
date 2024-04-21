d0a293db9c8e790045e2b1c2edcb1a0b8bc9d2c7
jalbrekt85 <jcalbrecht85@gmail.com>
2024-04-21 15:58:33 -0500

    debug without safe tx

--- tools/python/aura_snapshot_voting/vote.py
+++ tools/python/aura_snapshot_voting/vote.py
@@ -216,7 +216,7 @@ if __name__ == "__main__":
 
     calldata = Web3.keccak(text="signMessage(bytes)")[0:4] + encode(["bytes"], [hash])
 
-    post_safe_tx(vlaura_safe_addr, sign_msg_lib_addr, 0, calldata, Operation.DELEGATE_CALL)
+    # post_safe_tx(vlaura_safe_addr, sign_msg_lib_addr, 0, calldata, Operation.DELEGATE_CALL)
 
     # prep payload for relayer
     data["types"].pop("EIP712Domain")
