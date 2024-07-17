e1d5527ed1900618e7a1ed00cb98e95b23ea12bf
gosuto-inzasheru <gosuto-inzasheru@users.noreply.github.com>
2024-07-17 11:43:30 +0000

    style: ci lint with `black`

--- tools/python/query_upkeeps.py
+++ tools/python/query_upkeeps.py
@@ -4,19 +4,28 @@ from dune_client.types import QueryParameter
 from dune_client.query import QueryBase
 
 
-def get_upkeeps(chain='ethereum'):
+def get_upkeeps(chain="ethereum"):
     dune = DuneClient.from_env()
     query = QueryBase(
         name="@gosuto/cla_chain_upkeeps",
         query_id=3889683,
         params=[
-                QueryParameter.enum_type(name="chain", value=chain),
+            QueryParameter.enum_type(name="chain", value=chain),
         ],
     )
     return dune.run_query_dataframe(query)
 
+
 if __name__ == "__main__":
     dfs = []
-    for chain in ['ethereum', 'arbitrum', 'polygon', 'optimism', 'avalanche', 'base', 'gnosis']:
+    for chain in [
+        "ethereum",
+        "arbitrum",
+        "polygon",
+        "optimism",
+        "avalanche",
+        "base",
+        "gnosis",
+    ]:
         dfs.append(get_upkeeps(chain))
-    pd.concat(dfs).to_csv('../../out/upkeeps.csv', index=False)
+    pd.concat(dfs).to_csv("../../out/upkeeps.csv", index=False)
