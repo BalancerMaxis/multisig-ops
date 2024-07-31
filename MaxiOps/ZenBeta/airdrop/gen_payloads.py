c964e4bf931aa391061993c3034c252afa357d9f
jalbrekt85 <jalbrekt85@users.noreply.github.com>
2024-07-31 22:12:44 +0000

    style: ci lint with `black`

--- MaxiOps/ZenBeta/airdrop/gen_payloads.py
+++ MaxiOps/ZenBeta/airdrop/gen_payloads.py
@@ -8,21 +8,24 @@ import pandas as pd
 GNOSIS_OMNIBRIDGE = "0x88ad09518695c6c3712AC10a214bE5109a655671"
 GNOSIS_BAL_ADDRESS = "0x7eF541E2a22058048904fE5744f9c7E4C57AF717"
 
+
 def to_wei(amount):
     rounded = Decimal(amount).to_integral_value(rounding=ROUND_DOWN)
-    return int(rounded * Decimal('1e18'))
+    return int(rounded * Decimal("1e18"))
 
 
 airdrop = pd.read_csv("ZenBetaAirdrop.csv").dropna(subset=["Address"])
-mainnet_recipients = airdrop[(airdrop["Pay on mainnet"] == "Yes") & (airdrop["Total"] > 0)]
-gnosis_recipients = airdrop[(airdrop["Pay on mainnet"] != "Yes") & (airdrop["Total"] > 0)]
+mainnet_recipients = airdrop[
+    (airdrop["Pay on mainnet"] == "Yes") & (airdrop["Total"] > 0)
+]
+gnosis_recipients = airdrop[
+    (airdrop["Pay on mainnet"] != "Yes") & (airdrop["Total"] > 0)
+]
 bridge_to_gnosis = to_wei(gnosis_recipients["Total"].sum())
 
 
 ### mainnet transactions
-mainnet_builder = SafeTxBuilder(
-    "multisigs/lm", chain_name="mainnet"
-)
+mainnet_builder = SafeTxBuilder("multisigs/lm", chain_name="mainnet")
 bal = SafeContract("tokens/BAL", abi_file_path="abi/ERC20.json")
 omnibridge = SafeContract(GNOSIS_OMNIBRIDGE, abi_file_path="abi/omnibridge.json")
 
@@ -30,17 +33,13 @@ for _, row in mainnet_recipients.iterrows():
     bal.transfer(row["Address"], to_wei(row["Total"]))
 
 bal.approve(GNOSIS_OMNIBRIDGE, bridge_to_gnosis)
-omnibridge.relayTokens(
-    bal.address, AddrBook("gnosis").multisigs.lm, bridge_to_gnosis
-)
+omnibridge.relayTokens(bal.address, AddrBook("gnosis").multisigs.lm, bridge_to_gnosis)
 
 mainnet_builder.output_payload("transactions/ZenAirdropMainnet.json")
 
 
 ### gnosis transactions
-gnosis_builder = SafeTxBuilder(
-    "multisigs/lm", chain_name="gnosis"
-)
+gnosis_builder = SafeTxBuilder("multisigs/lm", chain_name="gnosis")
 bal = SafeContract(GNOSIS_BAL_ADDRESS, get_abi("ERC20"))
 
 for _, row in gnosis_recipients.iterrows():
