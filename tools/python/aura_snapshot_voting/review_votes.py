9abd5d43135871a4ce9058539c8368553c4c1761
jalbrekt85 <jalbrekt85@users.noreply.github.com>
2024-08-27 19:38:24 +0000

    style: ci lint with `black`

--- tools/python/aura_snapshot_voting/review_votes.py
+++ tools/python/aura_snapshot_voting/review_votes.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from bal_addresses.utils import to_checksum_address
 import requests
 
+
 def find_project_root(current_path=None):
     anchor_file = "multisigs.md"
     if current_path is None:
@@ -17,6 +18,7 @@ def find_project_root(current_path=None):
         raise FileNotFoundError("Project root not found")
     return find_project_root(parent)
 
+
 def fetch_gauge_labels():
     GAUGE_MAPPING_URL = "https://raw.githubusercontent.com/aurafinance/aura-contracts/main/tasks/snapshot/gauge_choices.json"
     response = requests.get(GAUGE_MAPPING_URL)
@@ -24,32 +26,35 @@ def fetch_gauge_labels():
     gauge_data = response.json()
     return {to_checksum_address(x["address"]): x["label"] for x in gauge_data}
 
+
 def review_votes(week_string):
     year, week = week_string.split("-")
     project_root = find_project_root()
     base_path = project_root / "MaxiOps/vlaura_voting"
     voting_dir = base_path / str(year) / str(week)
     input_dir = voting_dir / "input"
-    
+
     csv_files = glob.glob(str(input_dir / "*.csv"))
     if not csv_files:
         return "No CSV files found in the input directory."
 
     csv_file = csv_files[0]
     vote_df = pd.read_csv(csv_file)
-    
+
     vote_df = vote_df.dropna(subset=["Gauge Address", "Label", "Allocation %"])
-    
+
     gauge_labels = fetch_gauge_labels()
-    
-    vote_df["Checksum Address"] = vote_df["Gauge Address"].apply(lambda x: to_checksum_address(x.strip()))
+
+    vote_df["Checksum Address"] = vote_df["Gauge Address"].apply(
+        lambda x: to_checksum_address(x.strip())
+    )
     vote_df["Snapshot Label"] = vote_df["Checksum Address"].map(gauge_labels)
     missing_labels = vote_df[vote_df["Snapshot Label"].isna()]
     snapshot_label_check = len(missing_labels) == 0
-    
+
     total_allocation = vote_df["Allocation %"].str.rstrip("%").astype(float).sum()
     allocation_check = abs(total_allocation - 100) < 0.0001
-    
+
     report = f"""## vLAURA Votes Review
 
 CSV file: `{os.path.relpath(csv_file, project_root)}`
@@ -69,11 +74,12 @@ CSV file: `{os.path.relpath(csv_file, project_root)}`
 
 {"### ✅ All checks passed" if (allocation_check and snapshot_label_check) else "### ❌ Some checks failed"}
     """
-    
+
     with open("review_output.md", "w") as f:
         f.write(report)
-    
+
     return report
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Vote review script")
