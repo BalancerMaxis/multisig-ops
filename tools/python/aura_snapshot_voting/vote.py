430f025d647c2325bf616d543b63846a4d89faef
jalbrekt85 <jalbrekt85@users.noreply.github.com>
2025-03-28 03:34:31 +0000

    style: ci lint with `black`

--- tools/python/aura_snapshot_voting/vote.py
+++ tools/python/aura_snapshot_voting/vote.py
@@ -63,7 +63,6 @@ def post_safe_tx(safe_address, to_address, value, data, operation):
         to_address, value, data, operation, safe_nonce=nonce
     )
 
-    
     private_key = Account.from_mnemonic(PRIVATE_WORDS).key
     safe_tx.sign(private_key.hex()[2:])
 
